5864b21ab16ace2483a96523cbf29397e3ea4587
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-19 18:08:34 +0800

    feat: set margin

--- src/features/seats/SeatTable.js
+++ src/features/seats/SeatTable.js
@@ -1,34 +1,52 @@
 import React from "react";
 import { Row, Col } from "antd";
 import Seat from "./Seat";
+import SeatRowLabel from "./SeatRowLabel";
 import "./SeatTable.css";
 
 export default function SeatTable({ seatsIn2DList, onSeatClick }) {
   return (
     <div>
-      {seatsIn2DList.map((seatList, index) => (
-        <Row key={`seat-list${index}`}>
-          {seatList.map((seat, index) => {
-               let horizontalMarginClass = "";
-            if (index === 2 || index === 6 || index === 9) {
-              horizontalMarginClass += "rightMargin";
-            }
-            if (index === 0 || index === 3 || index === 7) {
-              horizontalMarginClass += " leftMargin";
-            }
-            return (
-              <Col key={seat.id} className={horizontalMarginClass}>
-                <Seat
-                  id={seat.id}
-                  column={seat.column}
-                  status={seat.status}
-                  onSeatClick={onSeatClick}
-                />
-              </Col>
-            );
-          })}
-        </Row>
-      ))}
+      {seatsIn2DList.map((seatList, rowIndex) => {
+        let verticalMarginClass = "";
+        if (rowIndex === 3) {
+          verticalMarginClass += "bottomMargin";
+        }
+
+        return (
+          <Row
+            key={`seat-list${rowIndex}`}
+            justify="center"
+            className={`row ${verticalMarginClass}`}
+          >
+            <Col>
+              <SeatRowLabel index={rowIndex} />
+            </Col>
+            {seatList.map((seat, columnIndex) => {
+              let horizontalMarginClass = "";
+              if (columnIndex === 2 || columnIndex === 6 || columnIndex === 9) {
+                horizontalMarginClass += "rightMargin";
+              }
+              if (columnIndex === 0 || columnIndex === 3 || columnIndex === 7) {
+                horizontalMarginClass += " leftMargin";
+              }
+              return (
+                <Col key={seat.id} className={horizontalMarginClass}>
+                  <Seat
+                    id={seat.id}
+                    column={seat.column}
+                    status={seat.status}
+                    onSeatClick={onSeatClick}
+                  />
+                </Col>
+              );
+            })}
+            <Col>
+              <SeatRowLabel index={rowIndex} />
+            </Col>
+          </Row>
+        );
+      })}
     </div>
   );
 }
