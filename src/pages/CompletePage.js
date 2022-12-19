2291355938fe41efbbb2e3894c38ca88fccc9b8b
tokaho <tokaho2000@gmail.com>
2022-12-19 20:30:34 +0800

    [Feat] add css on the status bar and the complete logo

--- src/pages/CompletePage.js
+++ src/pages/CompletePage.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import StatusBar from '../features/movie/StatusBar'
+import CompleteLogo from '../icons/CompleteLogo.png'
 export default function CompletePage() {
 
     return (
         <>
-        <div>
-            <StatusBar/>
+            <div>
+                <StatusBar stage={2} />
+                <div style={{
+                    "display": "flex",
+                    "flex-direction": "column",
+                    "align-items": "center",
+                    "margin-top": "50px"
+                }}>
+                    <img src={CompleteLogo} alt={"Ticket Reservated"} />
+                    <h1>Completed</h1>
+                </div>
             </div>
         </>
     )
