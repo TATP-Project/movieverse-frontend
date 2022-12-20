d492187c0ed3a2efa1635a878220994da8672296
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-20 14:38:03 +0800

    fix: fix duplicated store import

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { store } from "./app/store";
 import "antd/dist/reset.css";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
