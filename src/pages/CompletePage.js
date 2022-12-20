3cb6f1a3f747c4d7a5aaa5d34e037caf91b8c74a
tokaho <tokaho2000@gmail.com>
2022-12-20 10:07:16 +0800

    [Feat] update css, change font family to lato

--- src/pages/CompletePage.js
+++ src/pages/CompletePage.js
@@ -5,17 +5,15 @@ export default function CompletePage() {
 
     return (
         <>
-            <div>
-                <StatusBar stage={3} />
-                <div style={{
-                    "display": "flex",
-                    "flex-direction": "column",
-                    "align-items": "center",
-                    "margin-top": "50px"
-                }}>
-                    <img src={CompleteLogo} alt={"Ticket Reservated"} />
-                    <h1 style={{ 'font-family': 'Bebas Neue' }}>Completed</h1>
-                </div>
+            <StatusBar stage={3} />
+            <div style={{
+                "display": "flex",
+                "flex-direction": "column",
+                "align-items": "center",
+                "margin-top": "50px"
+            }}>
+                <img src={CompleteLogo} alt={"Ticket Reservated"} />
+                <h1 style={{ 'font-family': 'Bebas Neue' }}>Completed</h1>
             </div>
         </>
     )
