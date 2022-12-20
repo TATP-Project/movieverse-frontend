2c6312c787c651c0cdd29377aeac0fc8b1d20276
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-20 15:39:39 +0800

    feat: add status bar

--- src/pages/SeatSelectionPage.js
+++ src/pages/SeatSelectionPage.js
@@ -1,13 +1,12 @@
 import React from "react";
+import StatusBar from "../features/movie/StatusBar";
 import SeatSelection from "../features/seats/SeatSelection";
 
 export default function SeatSelectionPage() {
   return (
-    <>
-      <div className={"title"}>Now on Sale</div>
-      <div className={"body"}>
-        <SeatSelection />
-      </div>
-    </>
+    <div className={"body"}>
+      <StatusBar stage={1} />
+      <SeatSelection />
+    </div>
   );
 }
