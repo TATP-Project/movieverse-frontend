29eb7b2e63e8c546e9c5a61888bc42267fedb60a
leungpo <polly.leung@oocl.com>
2022-12-21 12:34:28 +0800

    feat: change BASE URL

--- src/api/movies.js
+++ src/api/movies.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:9999/",
-    baseURL: "https://movieverse-backend-staging.up.railway.app/",
+    baseURL: "https://movieverse-backend-production.up.railway.app/",
 });
 
 export const getMovies = () => {
