dfde84dfc5c2ae608b5387b5b74f8b33d5150377
tokaho <tokaho2000@gmail.com>
2022-12-21 16:26:40 +0800

    [Fix] fix css on old browsers

--- src/features/movie/StatusBar.js
+++ src/features/movie/StatusBar.js
@@ -8,17 +8,17 @@ const StatusBar = (props) => {
                     <div className="ellipse">1</div>
                     <p>Select Seat</p>
                 </div>
-                <div className='statusbar-line' />
+                <div className={'statusbar-line '+(props.stage>=2?'stage-completed':'stage-uncomplete')} />
                 <div className={"stage "+(props.stage>=2?'stage-completed':'stage-uncomplete')}>
                     <div className="ellipse">2</div>
                     <p>Food &amp; Beverage</p>
                 </div>
-                <div className='statusbar-line' />
+                <div className={'statusbar-line '+(props.stage>=3?'stage-completed':'stage-uncomplete')} />
                 <div className={"stage "+(props.stage>=3?'stage-completed':'stage-uncomplete')}>
                     <div className="ellipse">3</div>
                     <p>Order</p>
                 </div>
-                <div className='statusbar-line' />
+                <div className={'statusbar-line '+(props.stage>=4?'stage-completed':'stage-uncomplete')} />
                 <div className={"stage "+(props.stage>=4?'stage-completed':'stage-uncomplete')}>
                     <div className="ellipse">4</div>
                     <p>Complete</p>
