866f024d48ade28e47985e67d99a864dac1d43a9
leungpo <polly.leung@oocl.com>
2022-12-21 11:49:30 +0800

    feat: change env variable

--- src/api/movieSessions.js
+++ src/api/movieSessions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     // baseURL: "http://localhost:9999/",
     // baseURL: "https://movieverse-backend-staging.up.railway.app/",
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: process.env.BASE_URL,
 });
 
 export const getSeatsByMovieSessionId = (movieSessionId) => {
