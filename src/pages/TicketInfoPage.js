a38da166f9e8150e9da3e99d6cd3f5979ac9e83b
leungpo <polly.leung@oocl.com>
2022-12-23 00:27:02 +0800

    feat:add even trigger on the close page on ticket-info

--- src/pages/TicketInfoPage.js
+++ src/pages/TicketInfoPage.js
@@ -1,8 +1,17 @@
 import React from "react";
 import TicketInfo from "../features/ticketInfo/TicketInfo.js";
 import StatusBar from "../features/movie/StatusBar";
-
+import { useSelector } from "react-redux";
+import { updateSeatsByMovieSessionId } from "../api/movieSessions";
 export default function TicketInfoPage() {
+    const seats = useSelector((state) => state.seatSelection.seats);
+    const session = useSelector((state) => state.movieSession);
+    const seatToAvailable = seats.map((seat) => {
+        return { ...seat, status: "AVAILABLE" };
+    });
+    window.addEventListener('beforeunload', function (e) {
+        e.returnValue = updateSeatsByMovieSessionId(session.id, seatToAvailable);
+    });
     return (
         <div>
             <StatusBar stage={3} />
