46e1b07b3b2903e2613a24643963ca365e7f8a1b
tokaho <tokaho2000@gmail.com>
2022-12-23 00:00:02 +0800

    [Feat]  cancel  reservation  when back from critical section

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,15 +6,19 @@ import { ReactComponent as MovieLanguageLogo } from "../icons/Language.svg";
 import { ReactComponent as LocationLogo } from "../icons/Location.svg";
 import { ReactComponent as Logo } from "../icons/Logo.svg";
 import { useNavigate } from "react-router-dom";
+import { cleanCacheAndRedirect } from "../features/button/BackToHomeButton";
+import { useDispatch, useSelector } from "react-redux";
 
 
 export default function Header() {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const seatSelection = useSelector((state) => state.seatSelection);
     return (
         <div className={"header"}>
             <div></div>
             <div className={"centerDiv"}>
-                <Link to="/"  onClick={()=>{navigate('/');navigate(0);}}>
+                <Link to="/"  onClick={()=>{cleanCacheAndRedirect(navigate,dispatch,seatSelection)}}>
                     <Logo />
                 </Link>
             </div>
--- src/pages/FoodPage.js
+++ src/pages/FoodPage.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import FoodList from "../features/food/FoodList";
 import StatusBar from "../features/movie/StatusBar";
 import { useNavigate } from "react-router-dom";
+import BackToHomeButton from "../features/button/BackToHomeButton";
 
 export default function ListOfMoviesPage() {
     const history = useSelector((state) => state.history);
@@ -18,5 +19,5 @@ export default function ListOfMoviesPage() {
             <FoodList />
         </div>
     )
-    :<div className="sessionExpired"><p>Session Not Found/Expired</p><button onClick={()=>{navigate('/');navigate(0);}}>Back To Home</button></div> //incorrect history
+    :<BackToHomeButton/> //incorrect history
 }
