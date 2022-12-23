651f1dcb48e189397e702b49077ccde350146b82
lijo15 <lijo15@oocl.com>
2022-12-23 11:23:11 +0800

    fix: remove unused function

--- src/features/ticketInfo/TicketInfo.js
+++ src/features/ticketInfo/TicketInfo.js
@@ -22,21 +22,21 @@ export default function TicketInfo() {
     const dispatch = useDispatch();
     const targetDate = useSelector((state) => state.countdownTimer.targetDate);
 
-    const houseWordToNumber = (houseWord) => {
-        var number = {
-            one: 1,
-            two: 2,
-            three: 3,
-            four: 4,
-            five: 5,
-            six: 6,
-            seven: 7,
-            eight: 8,
-            nine: 9,
-            ten: 10,
-        };
-        return number[houseWord.split(" ").pop().toLowerCase()];
-    };
+    // const houseWordToNumber = (houseWord) => {
+    //     var number = {
+    //         one: 1,
+    //         two: 2,
+    //         three: 3,
+    //         four: 4,
+    //         five: 5,
+    //         six: 6,
+    //         seven: 7,
+    //         eight: 8,
+    //         nine: 9,
+    //         ten: 10,
+    //     };
+    //     return number[houseWord.split(" ").pop().toLowerCase()];
+    // };
 
     const handleConfirmTicketInfo = () => {
         var foodList = [];
